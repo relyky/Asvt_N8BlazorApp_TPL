679b6dc91b2e07bb6ae3cf6a0843b2183fddfb83
Rely Ky <relyky@hotmail.com>
2025-04-30 18:39:35 +0800

    簡單托拉

---
+++ Vista.Biz/DEMO/DEMO209Biz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vista.AOP;
+
+namespace Vista.Biz.DEMO;
+
+[CatchAndLog("拖拉編輯展示二Biz")]
+class DEMO209Biz
+{
+}
+
+public class CommentItem
+{
+  public int Order { get; set; }
+  public string Comment { get; set; } = string.Empty;
+
+  public CommentItem Clone()
+  {
+    return new CommentItem
+    {
+      Order = this.Order,
+      Comment = this.Comment
+    };
+  }
+}
